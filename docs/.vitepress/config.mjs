f9f0c1332196016aa3617ffab26a4f1eaba612d6
guanhai <q2274799107@126.com>
2025-03-13 13:23:32 +0800

    chore(add config): 添加gitLab page

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -2,43 +2,62 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  // 站点的标题。
   title: "学习webpack",
+  // 站点的描述。这将呈现为页面 HTML 中的 <meta> 标签。
   description: "一个学习webpack的文档站点",
+  // 主题配置，用于自定义主题， https://vitepress.dev/reference/default-theme-config
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    // 导航菜单配置，参考：https://vitepress.dev/zh/reference/default-theme-nav
     nav: [
       { text: "Home", link: "/" },
-      { text: "Demo", link: "/Demo" }
+      { text: "Demo", link: "/Demo" },
     ],
+    // 大纲容器, 参考：https://vitepress.dev/zh/reference/default-theme-config#outline
     outline: {
       level: [2, 6],
-      label: '页面导航'
+      label: "页面导航",
     },
+    // 侧边栏菜单项的配置，参考：https://vitepress.dev/zh/reference/default-theme-sidebar
     sidebar: [
       {
         text: "前言",
-        link: "/前言"
+        link: "/前言",
       },
       {
         text: "第一章 核心功能",
-        link: "/第一章文档"
+        link: "/第一章文档",
       },
       {
         text: "第二章 常用扩展",
         link: "/第二章文档",
-      }
+      },
     ],
+    // 可以定义此选项以在导航栏中展示带有图标的社交帐户链接，参考：https://vitepress.dev/zh/reference/default-theme-config#sociallinks
     socialLinks: [
       {
         icon: "gitlab",
         link: "http://code.thylovezq.site:30086/YangHai/learn-webpack",
       },
     ],
+    // 允许自定义上次更新的文本和日期格式，参考：https://vitepress.dev/zh/reference/default-theme-config#lastupdated
+    lastUpdated: {
+      text: "上次更新在",
+      formatOptions: {
+        dateStyle: "short",
+        timeStyle: "short",
+      },
+    },
+    // 支持搜索站点文档，参考：https://vitepress.dev/zh/reference/default-theme-search
+    search: {
+      provider: "local",
+    },
   },
-  lastUpdated: true,
   markdown: {
     image: {
-      lazyLoading: true
-    }
-  }
+      lazyLoading: true,
+    },
+  },
+  outDir: "../public",
+  base: "/learn-webpack/",
 });
