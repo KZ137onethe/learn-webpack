9f4cef66542c84631f75f636e16aa65f2cf6cf12
guanhai <q2274799107@126.com>
2025-03-22 19:01:58 +0800

    delete: 删除不需要的文件

--- 第2章 常用扩展/01-自动生成页面/dist/about/index.83d0f.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!************************!*\
-  !*** ./about/index.js ***!
-  \************************/
-console.log("module about");
-
-/******/ })()
-;
-//# sourceMappingURL=index.83d0f.js.map
--- 第2章 常用扩展/01-自动生成页面/dist/home/index.4f9e4.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-/*!***********************!*\
-  !*** ./home/index.js ***!
-  \***********************/
-console.log("module home");
-
-/******/ })()
-;
-//# sourceMappingURL=index.4f9e4.js.map
