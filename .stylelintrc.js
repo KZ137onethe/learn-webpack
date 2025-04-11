81f304fed1c25089ae04ab0a33107f137c5692f0
guanhai <q2274799107@126.com>
2025-04-11 23:47:41 +0800

    chore(stylelint): 添加stylelint忽略文件夹的配置

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   extends: ["stylelint-config-standard"],
+  ignoreFiles: ["node_modules", "**/node_modules/**", "dist", "**/dist/**"],
   // 规则参考：https://stylelint.nodejs.cn/user-guide/rules
   rules: {},
 };
