3be19753d34da9437db96543c568e003f73e5b11
guanhai <q2274799107@126.com>
2025-04-16 13:29:50 +0800

    chore(stylelint): 添加css,scss的文件检查

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,13 @@
 module.exports = {
-  extends: ["stylelint-config-standard"],
+  extends: ["stylelint-config-standard-scss"],
   ignoreFiles: ["node_modules", "**/node_modules/**", "dist", "**/dist/**"],
   // 规则参考：https://stylelint.nodejs.cn/user-guide/rules
-  rules: {},
+  rules: {
+    "selector-class-pattern": [
+      "^[a-z0-9]+(?:-[a-z0-9]+)*(?:__[a-z0-9]+(?:-[a-z0-9]+)*)?(?:--[a-z0-9]+(?:-[a-z0-9]+)*)?$",
+      {
+        message: "类名需符合BEM规范（如：block__element--modifier）",
+      },
+    ],
+  },
 };
