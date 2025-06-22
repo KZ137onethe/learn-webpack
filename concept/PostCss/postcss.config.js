a3838ce3d4e260a396f2d2559aafc3948331c9b8
guanhai <q2274799107@126.com>
2025-06-22 23:32:54 +0800

    docs: 补充第四章CSS工程化中CSS标准化流程的文档

--- concept/PostCss/postcss.config.js
+++ concept/PostCss/postcss.config.js
@@ -11,14 +11,16 @@ module.exports = {
   plugins: [
     // 请参阅 https://github.com/csstools/postcss-plugins/tree/main/plugin-packs/postcss-preset-env#readme 查看
     postcssPresetEnv({
-      // stage选项根据CSS特性在成为实现的web标准过程中的稳定性来确定要填充哪些CSS特性。
+      // stage依据于cssdb，默认为stage：2，stage选项根据CSS特性在成为实现的web标准过程中的稳定性来确定要填充哪些CSS特性。
       stage: 3,
-      // features选项按ID启用或禁用特定的polyfill,参考：https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
+      // features选项按ID 手动启用或禁用特定的polyfill,参考：https://github.com/csstools/postcss-plugins/blob/main/plugin-packs/postcss-preset-env/FEATURES.md
       features: {
-        // 该语法在草案阶段(stage: 0)
+        // 该语法在草案阶段(stage: 1)
         "custom-selectors": true,
         // 支持嵌套(stage: 2)
         "nesting-rules": true,
+        // 重置元素所有属性的属性"all"(stage: 4)
+        "all-property": false,
       },
       // 可以是插件接受该值，默认为配置（如果置为false，那么只会编译成旧语法）
       preserve: false,
